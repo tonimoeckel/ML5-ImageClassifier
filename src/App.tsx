595ecebc1be19fecd70cefec9bd13a11a53aad2b
Toni Möckel <toni.moeckel@dreebit.com>
2023-10-15 22:50:17 +0200

    Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {MLImageCard} from "./components/MLImageCard";
+import {ImageClassifierProvider} from "./provider/MLProvider";
+import {Chart, ArcElement} from 'chart.js'
+import {MLUploadCard} from "./components/MLUploadCard";
+Chart.register(ArcElement);
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ImageClassifierProvider>
+            <div style={{display: 'flex', gap: 16, flexDirection: 'column' }}>
+                <h1 style={{textAlign: 'center'}}>Image Classifier</h1>
+                <h2 style={{textAlign: 'center'}}>Good</h2>
+                <div style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', alignContent: 'center' , display: 'flex', flexWrap: "wrap", gap: 16}}>
+                    <MLImageCard key={"panda"} src={"/assets/IMG_2634.JPG"} />
+                    <MLImageCard key={"tiki"} src={"/assets/DSC00358.jpg"} />
+
+
+                </div>
+
+                <h2 style={{textAlign: 'center'}}>Mix</h2>
+                <div style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', alignContent: 'center' , display: 'flex', flexWrap: "wrap", gap: 16}}>
+                    <MLImageCard key={"bird"} src={"/assets/IMG_0004.JPG"} />
+                    <MLImageCard key={"schneeleopard"} src={"/assets/IMG_2681.JPG"} />
+                    <MLImageCard key={"arabica"} src={"/assets/216602-00-GPIE.jpg"} />
+                </div>
+
+
+                <h2 style={{textAlign: 'center'}}>Bad</h2>
+                <div style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', alignContent: 'center' , display: 'flex', flexWrap: "wrap", gap: 16}}>
+                    <MLImageCard key={"faultier"} src={"/assets/IMG_2644.JPG"} />
+                    <MLImageCard key={"tiki2"} src={"/assets/IMG001.jpg"} />
+                    <MLImageCard key={"panda"} src={"/assets/Pizza_salami_slices.jpg"} />
+                </div>
+
+                <h2 style={{textAlign: 'center'}}>Yours</h2>
+                <div style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', alignContent: 'center' , display: 'flex', flexWrap: "wrap", gap: 16}}>
+                    <MLUploadCard key={"4"} />
+                </div>
+            </div>
+
+
+        </ImageClassifierProvider>
     </div>
   );
 }
---
+++ src/components/MLImageCard/index.tsx
@@ -0,0 +1,88 @@
+import "./index.css";
+import {useImageClassifier} from "../../provider/MLProvider";
+import {useEffect, useRef, useState} from "react";
+import {Pie} from "react-chartjs-2";
+
+interface ClassificationResult {label: string, confidence: number}
+
+const loadImage = (src: string) =>
+    new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = reject;
+        img.src = src;
+    })
+;
+
+export const MLImageCard: React.FC<{
+    src: string;
+}> = props => {
+
+    const [resultIndex, setResultIndex] = useState(0)
+    const { classifier } = useImageClassifier();
+    const imageRef = useRef<any>();
+    const [classifications, setClassifications] = useState<ClassificationResult[]>([])
+
+    const doIt = async () => {
+        const image = await loadImage(props.src);
+        classifier.classify(image, (err: any, classifications: ClassificationResult[]) => {
+
+            if (!err) {
+                setResultIndex(0);
+                setClassifications(classifications);
+            }
+
+        })
+    }
+
+    useEffect(() => {
+
+        doIt();
+
+
+    }, [imageRef, props.src]);
+
+    const classification = classifications[resultIndex];
+
+    return (
+        <div style={{display: 'flex', position: 'relative', flexDirection: 'row', alignItems: 'center', gap: 16}}>
+            <div className={"image-card"}>
+                <img ref={imageRef} className={"image-card__img"} src={props.src} />
+                <span className="image-card__footer">
+                {classification ? <span>{classification.label}</span> : null}
+                    {classification ? <span>{classification.confidence}</span> : null}
+                    {classification ? <span onClick={() => {
+                        if ((resultIndex + 1) < classifications.length) {
+                            setResultIndex(resultIndex + 1)
+                        }else {
+                            setResultIndex(0)
+                        }
+                    }} className="image-card__action">
+    <svg viewBox="0 0 448 512">
+      <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+    </svg>
+  </span> : null}
+          </span>
+            </div>
+
+            {classification ?
+                <div style={{width: 50, position: 'absolute', right: 20, top: 20}}>
+                    <Pie
+                        data={{
+                            datasets: [{
+                                backgroundColor: [
+                                    'rgb(35,222,8)',
+                                    'rgb(234,36,36)',
+                                ],
+                                label: 'My First Dataset',
+                                data: [classification.confidence, 1-classification.confidence],
+                            }]
+                        }}
+                    />
+                </div>
+                : null}
+
+        </div>
+
+    );
+};
